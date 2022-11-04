3a392a06a7cf9f9e3e6e841270fadd9a8753be7d
Raj-techwondoe <raj.upadhyay@techwondoe.com>
2022-11-04 17:35:03 +0530

    Molecule documentation and name update to Kickass UI

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,12 +1,5 @@
 import {
-  Radio,
   Typography,
-  Button,
-  Avatar,
-  Toggle,
-  Accordion,
-  Slider,
-  ListWithRadioOnRight,
   extendTheme,
   base,
   ThemeProvider,
@@ -26,84 +19,9 @@ const customTheme = extendTheme(base, {
 const App = () => {
   return (
     <ThemeProvider customTheme={customTheme}>
-      <div style={{ padding: 20 }}>
-        <Typography color="primary" variant="h1">
-          test
-        </Typography>
-        <Typography color="secondary" variant="h2">
-          test
-        </Typography>
-        <Typography color="success" variant="h3">
-          test
-        </Typography>
-        <Typography color="warning" variant="h4">
-          test
-        </Typography>
-        <Typography color="danger" variant="h5">
-          test
-        </Typography>
-        <Button
-          onClick={() => {
-            console.info('type 1 pressed');
-          }}
-          variant="contained">
-          test the button
-        </Button>
-        <Button
-          onClick={() => {
-            console.info('type 2 pressed');
-          }}
-          variant="outlined"
-          color="success">
-          test the button
-        </Button>
-        <Button
-          onClick={() => console.log('pressed')}
-          variant="outlined"
-          size="medium"
-          color="onSurface">
-          test the button
-        </Button>
-        <Avatar alt={''} src={'https://avatars.githubusercontent.com/u/75943412?v=4'} />
-        <Toggle id={'test'} name={'test toggle'} />
-        <Accordion
-          accordoinData={[
-            {
-              title: 'Accordion 1',
-              content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
-            },
-            {
-              title: 'Accordion 2',
-              content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.',
-            },
-          ]}
-        />
-        <ListWithRadioOnRight
-          accounts={[
-            { id: 'checking', name: 'Checking', description: 'CIBC ••••6610' },
-            {
-              id: 'savings',
-              name: 'Savings',
-              description: 'Bank of America ••••0149',
-            },
-            {
-              id: 'mastercard',
-              name: 'Mastercard',
-              description: 'Capital One ••••7877',
-            },
-          ]}
-          title={'List with radio'}
-        />
-        <div className="radios">
-          <Radio id="name" name="my-radios" title="Name" onChange={() => {}} />
-          <Radio id="last-name" name="my-radios" title="Last Name" onChange={() => {}} />
-        </div>
-        <div className="slider">
-          <Slider size="medium" color="primary" step={1} min={0} max={100} />
-        </div>
-      </div>
+      <Typography color="primary" variant="h1">
+        Kickass UI Example project
+      </Typography>
     </ThemeProvider>
   );
 };

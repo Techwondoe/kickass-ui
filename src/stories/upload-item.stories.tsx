53d35ab7d6743eeb2bc95906f4d96c79b6bbaa1a
YoitsVansh <vansh.199301021@muj.manipal.edu>
2023-03-07 14:07:46 +0530

    Fixed Upload Item component

---
+++ src/stories/upload-item.stories.tsx
@@ -0,0 +1,20 @@
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
+import { UploadItem } from "../components/atoms/uploadItem/upload-item";
+
+const Story: ComponentMeta<typeof UploadItem> = {
+    component: UploadItem,
+    title: "Forms/Upload Item",
+};
+export default Story;
+
+const Template: ComponentStory<typeof UploadItem> = args => (
+    <UploadItem {...(args as any)} />
+);
+
+export const Regular = Template.bind({});
+Regular.args = {
+    label: "Design1.psd",
+    size: "5.2 mb",
+    progress: 20
+};

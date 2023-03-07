fa1e1f9f2389984ee4b86d068e5c3c005b0af91d
YoitsVansh <vansh.199301021@muj.manipal.edu>
2023-03-07 11:48:51 +0530

    Added Phone Input, Search Input, Select Input, Site Input components

---
+++ src/stories/phone-input.stories.tsx
@@ -0,0 +1,19 @@
+import type { ComponentStory, ComponentMeta } from "@storybook/react";
+import React from "react";
+import { PhoneInputField } from "../components/atoms/PhoneInput/phone-input";
+
+const Story: ComponentMeta<typeof PhoneInputField> = {
+    component: PhoneInputField,
+    title: "Forms/Phone Input Field",
+};
+export default Story;
+
+const Template: ComponentStory<typeof PhoneInputField> = args => (
+    <PhoneInputField {...args} />
+);
+
+export const Regular = Template.bind({});
+Regular.args = {
+    label: "Phone",
+    hint: "This is a hint text to help user.",
+};

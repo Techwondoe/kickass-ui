df28fae8a4294b17bd7a6aac66d9ab6b46b78a0f
YoitsVansh <vansh.gupta@techwondoe.com>
2023-03-30 14:56:20 +0530

    Fixed upload component: Drag and Drop Functionality working with min-max size

--- src/components/atoms/upload/upload.types.ts
+++ src/components/atoms/upload/upload.types.ts
@@ -1,11 +1,20 @@
 /**
- * @param accept - Accept string for component
+ * @param maxFiles - Maximum number of files that can be accepted
+ * @param maxSize - Maximum size of file that can be accepted
+ * @param minSize - Minimum size of file that can be accepted
+ * @param acceptedValues - Accept string for component
  * @param disabled - Whether the component is disabled or not
  * @param onChange - OnChange handler for the component
  */
 
 export interface UploadProps {
-  accept?: string;
+  maxFiles?: number;
+  maxSize?: number;
+  minSize?: number;
+  acceptedValues?: string[];
   disabled?: boolean;
-  onChange?: (fileList: FileList | null) => void;
+  onError?: (err: unknown) => void;
+  onChange?: (fileList: File[] | null) => void;
 }
+
+
